c8050a03c0e219e57def4563e51854d202d47e69
VitaliyGladkiy <vgladkiy89@gmail.com>
2020-07-02 00:22:59 +0300

    feature(step 3):   - add read stream sync func

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env node
+const getUserNameFromStream = require('./src/cli').getUserNameFromStream;
+
 console.log('Welcome to the Brain Games');
+const userName = getUserNameFromStream();
+console.log(`Hello, ${userName}`);
---
+++ bin/src/cli.js
@@ -0,0 +1,6 @@
+let readlineSync = require('readline-sync');
+function getUserNameFromStream () {
+    return readlineSync.question('May I have your name? ');
+}
+
+module.exports.getUserNameFromStream = getUserNameFromStream;
